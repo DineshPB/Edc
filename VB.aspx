c16e1f3428003c82256c9bca50ddaa56b4de1648
DineshPB <prajapatidinesh869@gmail.com>
2017-03-30 23:31:24 +0530

    Freshen Up!

---
+++ VB.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="VB.aspx.vb" Inherits="VB" %>
+<%@ Register Src = "~/UC_DateSelectorVB.ascx" TagName = "DateSelector" TagPrefix = "uc" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+     <uc:DateSelector ID = "ucDateSelector" runat = "server" />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Button" OnClick  = "Submit" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ VB.aspx.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Data.SqlClient
+
+Partial Class VB
+    Inherits System.Web.UI.Page
+
+    Protected Sub Submit(ByVal sender As Object, ByVal e As EventArgs) Handles Button1.Click
+        Response.Write(("You selected " + ucDateSelector.SelectedDate.ToShortDateString))
+        Dim a = ucDateSelector.SelectedDate
+        Dim str As String
+        str = "Data Source=.\SQLEXPRESS;AttachDbFilename=H:\datepicker\App_Data\Database.mdf;Integrated Security=True;User Instance=True"
+        Dim cn As New SqlConnection(str)
+        Dim q As String = "Insert into [Table1] (date) values ('" + ucDateSelector.SelectedDate + "')"
+        Dim cmd As New SqlCommand(q, cn)
+        cn.Open()
+        Dim dr As SqlDataReader
+        dr = cmd.ExecuteReader()
+        cn.Close()
+    End Sub
+
+
+    'Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+    '    If Not IsPostBack Then
+    '        ucDateSelector.SelectedDate = DateTime.Parse("05/10/2010")
+    '    End If
+    'End Sub
+End Class
